d3cab5dba56d8a06d69d74fa348e45beb5e86790
Gian Santillan <giansantillan18@gmail.com>
2014-02-10 15:26:39 +0800

    init

--- src/Lightshire/Paypal/Paypal.php
+++ src/Lightshire/Paypal/Paypal.php
@@ -3,16 +3,35 @@
 	namespace Lightshire\Paypal;
 
 	use App;
+	use Paypal\Auth\OAuthTokenCredential;
+
 
 	class Paypal
 	{
+		private $creds = null;
+		private $token = null;
+
 		public function __construct()
 		{
+			$data = Paypal::launch();
+			$this->creds = $data["creds"];
+			$this->token = $data["token"];
 			
 		}
 
-		public static function createBuyButton()
+		public static function launch()
 		{
-			
+			$client_id 	= Config::get('laravel-paypal:client_id');
+			$secret 	= Config::get('laravel-paypal:secret');
+			$mode 		= Config::get('laravel-paypal:mode');
+
+			$creds 			= new OAuthTokenCredential($client_id, $secret);
+
+			$accessToken 	= $creds->getAccessToken(array('mode'=>$mode));	 
+		
+			return array(
+					'creds' 	=> $creds,
+					'token' 	=> $token
+				);
 		}
 	}
